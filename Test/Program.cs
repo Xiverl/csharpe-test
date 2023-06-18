2b9b53ccd314967dd6d28310a6198573bb4ac6db
Xiverl <den.elynin0903@gmail.com>
2023-06-18 12:36:25 +0400

    Добавил проверку на полиндром.

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,5 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Введите слово: ");
-string word = Console.ReadLine().ToString();
-Console.WriteLine("Вот твое слово: " + word);
-Console.ReadKey();
+using static System.Console;
+
+
+static bool isPolindrom(string word)
+{
+    char[] reverse_arr = word.ToCharArray();
+    Array.Reverse(reverse_arr);
+    string reverse_word = new string(reverse_arr);
+    if (reverse_word == word)
+        return true;
+    return false;
+}
+
+WriteLine("Введите слово: ");
+string word = ReadLine().ToString();
+if (isPolindrom(word))
+    WriteLine("Это слово - полиндром");
+else
+    WriteLine("Это слово - НЕ полнидром");
+ReadKey();
