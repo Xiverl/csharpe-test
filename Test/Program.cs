dc85ce048e3bb2d440311171b28c64b9793496aa
Xiverl <den.elynin0903@gmail.com>
2023-06-18 12:01:39 +0400

    Исходный код.

---
+++ Test/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите слово: ");
+string word = Console.ReadLine().ToString();
+Console.WriteLine("Вот твое слово: " + word);
+Console.ReadKey();
